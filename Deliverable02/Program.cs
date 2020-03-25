a0fba11d9d9a4ec1f08165a88457345b47a3a181
andy <asmithdev91@gmail.com>
2020-03-24 23:24:04 -0400

    first commit for deliverable two

---
+++ Deliverable02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Deliverable02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "", message = "";
+            int checksum = 0;
+
+            Console.Write("What is your message? ");
+            input = Console.ReadLine();
+            input = input.ToUpper();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int u = (int)input[i];
+                checksum += u;
+                message += u - 64;
+                if (i != (input.Length - 1))
+                {
+                    message += "-";
+                }
+            }
+
+            Console.WriteLine("Your encoded message is " + message);
+            Console.WriteLine("Message checksum is " + checksum);
+        }
+    }
+}
